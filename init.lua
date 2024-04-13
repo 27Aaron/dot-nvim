db5e66ee433121cbb999c4e05215f1671113460d
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-14 00:37:05 +0800

    feat: add comments

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
 local opt = vim.opt
-opt.signcolumn = 'yes'
-opt.number = true
-opt.relativenumber = true
+
+-- global options
+opt.signcolumn = 'yes'                      -- always show signcolumn
+opt.number = true                           -- show line numbers
+opt.relativenumber = true                   -- show relative line numbers
 
 -- Clipboard & Mouse
-opt.mouse:append 'a'
-opt.clipboard:append 'unnamedplus'
+opt.mouse:append 'a'                        -- enable mouse support
+opt.clipboard:append 'unnamedplus'          -- use system clipboard
+
