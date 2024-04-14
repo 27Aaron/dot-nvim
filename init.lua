644e93fd3baaa890f001228e43a0c560b038812b
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-14 13:12:49 +0800

    feat: add nvim-lspconfig

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ require("lazy").setup({
 		event = "VeryLazy",
 		"williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
 		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
 		config = function()
 			require("mason").setup()
