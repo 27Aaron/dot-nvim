e83823a7c20cebbf1dd263c0798edbc77017490f
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-14 18:10:57 +0800

    refactor(lua): mason.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -10,6 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 -- require("lazy").setup({
