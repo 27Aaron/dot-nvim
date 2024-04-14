f9a23f3b804b1d1eab907f69867843625d303231
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-14 17:56:06 +0800

    refactor(plugins): colorscheme.lua

--- init.lua
+++ init.lua
@@ -32,22 +32,22 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	"folke/neodev.nvim",
-	{ "neanias/everforest-nvim", version = false, lazy = false, priority = 1000, },
-  { cmd = "Telescope", 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
-	{
-		event = "VeryLazy",
-		"williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-		config = function()
-			require("mason").setup()
-		end,
-	},
-  })
+-- require("lazy").setup({
+-- 	"folke/which-key.nvim",
+-- 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+-- 	"folke/neodev.nvim",
+--   { cmd = "Telescope", 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
+-- 	{
+-- 		event = "VeryLazy",
+--     "williamboman/mason.nvim",
+--     "williamboman/mason-lspconfig.nvim",
+--     "neovim/nvim-lspconfig",
+-- 		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+-- 		config = function()
+-- 			require("mason").setup()
+--       require("mason-lspconfig").setup()
+-- 		end,
+-- 	},
+--   })
 
-  vim.cmd.colorscheme("everforest")
+require('lazy').setup 'plugins'
