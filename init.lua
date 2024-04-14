2ee71dd4a4299af16242dd3db2dc8ffe7e047535
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-14 11:40:44 +0800

    feat: add colorscheme

--- init.lua
+++ init.lua
@@ -36,4 +36,7 @@ require("lazy").setup({
 	"folke/which-key.nvim",
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",
+	{ "neanias/everforest-nvim", version = false, lazy = false, priority = 1000, },
   })
+
+  vim.cmd.colorscheme("everforest")
