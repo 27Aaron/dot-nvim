e0e7033e598af5aed32856611dd8e8aee411b0c8
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-18 13:19:29 +0800

    refactor: start from scratch

--- lua/plugins/copilot.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'github/copilot.vim',
-  config = function()
-    vim.g.copilot_proxy = 'http://127.0.0.1:6152'
-  end,
-}
--- lua/plugins/other.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'mg979/vim-visual-multi', -- Multiple cursors
-  { 'wakatime/vim-wakatime', lazy = false }, -- WakaTime
-}
