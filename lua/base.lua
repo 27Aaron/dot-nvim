6671929ddbb7cc7cdcffd0cc88e54fd3818d19a8
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-25 14:37:46 +0800

    feat(base): add setting (#3)

---
+++ lua/base.lua
@@ -0,0 +1,37 @@
+local opt = vim.opt
+
+-- global options
+opt.signcolumn = 'yes' -- always show signcolumn
+opt.number = true -- show line numbers
+opt.relativenumber = true -- show relative line numbers
+
+-- window options
+opt.scrolloff = 5 -- keep 5 lines above/below cursor
+opt.sidescrolloff = 5 -- keep 5 lines left/right of cursor
+
+opt.tabstop = 2 -- tab width
+opt.shiftwidth = 2 -- number of spaces to use for autoindent
+opt.expandtab = true -- use spaces instead of tabs
+opt.autoindent = true -- maintain indent of current line
+
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- ignore case if search pattern is all lowercase
+
+-- Improve performance
+opt.swapfile = false -- disable swap file
+opt.autoread = true -- auto read file when changed outside of vim
+vim.bo.autoread = true -- auto read file when changed outside of vim
+
+-- Clipboard & Mouse
+opt.mouse:append 'a' -- enable mouse support
+opt.clipboard:append 'unnamedplus' -- use system clipboard
+
+-- enable true colors
+opt.termguicolors = true
+
+-- highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
