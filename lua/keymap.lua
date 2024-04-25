ce467ab0c0a1ab1bf87da026de647d28ad18e86f
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-25 17:08:34 +0800

    style: format & indent
    Co-authored-by: 27Aaron <85681241+27aaron@users.noreply.github.com>

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,3 +13,4 @@ keymap.set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
 keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
 
 keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
+
