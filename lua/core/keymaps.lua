4a0602dce499e0fdfa43ffe6f1bd2f44182fa3f2
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-08-31 20:36:44 +0800

    feat: Select all the contents

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,3 +10,5 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+keymap.set('n', '<c-a>', 'ggVG', { desc = "Select all the contents" })
