354f527b97562f938693d15288c2f454378c0d2f
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-08-31 20:14:42 +0800

    feat: add lazy.nvim

--- init.lua
+++ init.lua
@@ -9,4 +9,21 @@
 
 -- Author: Aaron (https://github.com/27Aaron/dot-nvim)
 
-require 'core'
+-- lazy.nvim
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('core')
+require('lazy').setup('plugins')
