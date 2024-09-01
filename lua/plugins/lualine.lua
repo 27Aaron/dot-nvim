06050c0f05a0807a5cc315def89d7936a7e2b17e
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-09-01 15:44:44 +0800

    feat: add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = true,
+      colorscheme = "everforest",
+    },
+  },
+}
