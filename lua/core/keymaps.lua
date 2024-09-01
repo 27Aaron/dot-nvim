a30ec7f10fc84564b848b7acbe5142574c18e283
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-09-01 11:12:36 +0800

    refactor: start from scratch

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,20 +1,15 @@
 vim.g.mapleader = ' '
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set('n', '<c-a>', 'ggVG')
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
-
-keymap.set('n', '<c-a>', 'ggVG', { desc = "Select all the contents" })
-
-keymap.set({ 'n', 'x' }, '<leader>p', '"0p', { desc = "Unnamed register location paste" })
+keymap.set({ 'n', 'x' }, '<leader>p', '"0p')
 
 keymap.set('n', '<leader>q', '<cmd>q<cr>')
 keymap.set('n', '<leader>w', '<cmd>w<cr>')
 keymap.set('n', '<leader>x', '<cmd>x<cr>')
+
+keymap.set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
+keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
+
+keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
